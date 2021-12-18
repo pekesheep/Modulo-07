a8d0c5d58db741cb59f56eddc54b14c034ea163e
unknown <pekesheep@gmail.com>
2021-12-18 20:19:03 +0100

    Laboratorio 07

--- script.js
+++ script.js
@@ -1,122 +1,59 @@
-// Constantes.
-const REGULAR_TYPE = 21;
-const LOWER_TYPE = 4;
-const EXEMPT_TYPE = 0;
-// Entrada.
-var products = [
+// Constantes
+var WORK_HOURS = [
+  "08:00 - 09:00",
+  "09:00 - 10:00",
+  "10:00 - 11:00",
+  "11:00 - 12:00",
+  "12:00 - 13:00",
+  "13:00 - 14:00",
+  "15:00 - 16:00",
+  "16:00 - 17:00"
+];
+
+// Datos
+var myTeam = [
   {
-    description: "Goma de borrar",
-    price: 0.25,
-    tax: LOWER_TYPE,
-    stock: 2,
-    units: 0,
+    name: "María",
+    availability: new Array(8).fill(true)
   },
   {
-    description: "Lápiz H2",
-    price: 0.4,
-    tax: LOWER_TYPE,
-    stock: 5,
-    units: 0,
+    name: "Pedro",
+    availability: new Array(8).fill(true)
   },
   {
-    description: "Cinta rotular",
-    price: 9.3,
-    tax: REGULAR_TYPE,
-    stock: 2,
-    units: 0,
+    name: "Esther",
+    availability: new Array(8).fill(true)
   },
   {
-    description: "Papelera plástico",
-    price: 2.75,
-    tax: REGULAR_TYPE,
-    stock: 5,
-    units: 0,
-  },
-  {
-    description: "Escuadra",
-    price: 8.4,
-    tax: REGULAR_TYPE,
-    stock: 3,
-    units: 0,
-  },
-  {
-    description: "Pizarra blanca",
-    price: 5.95,
-    tax: REGULAR_TYPE,
-    stock: 2,
-    units: 0,
-  },
-  {
-    description: "Afilador",
-    price: 1.2,
-    tax: LOWER_TYPE,
-    stock: 10,
-    units: 0,
-  },
-  {
-    description: "Libro ABC",
-    price: 19,
-    tax: EXEMPT_TYPE,
-    stock: 2,
-    units: 0,
+    name: "Marcos",
+    availability: new Array(8).fill(true)
   },
 ];
 
-//Hay mucho que pulir porque el control de entrada de si los input valen 0 o no, no está bien del todo.
-//Si haces un test metiendo valores y borrando se vuelve loco el programa :'(
-
-var showProducts = (productList) => {
-  var container = document.getElementById("product-list-container");
-  var inputContainer = document.getElementById("product-input-container");
-
-  var createProduct = (product) => {
-    var description = document.createElement("div");
-    var quantity = document.createElement("input");
-
-    description.innerText = `${product.description} - ${product.price} €/ud.`;
-
-    description.setAttribute("class", "product-list-description");
-    quantity.setAttribute("type", "number");
-    quantity.setAttribute("class", "product-input");
-    quantity.addEventListener("change", (event) => {
-      product.units = event.target.valueAsNumber;
-
-      document.getElementById("button-calculate").disabled =
-        product.units == 0 ? true : false;
-    });
-
-    container.appendChild(description);
-    inputContainer.appendChild(quantity);
-  };
-
-  for (var product of productList) {
-    createProduct(product);
+var randomSchedule = (team, hours) => {
+  for (var member of team) {
+    console.log("Disponibilidad de", member.name);
+    for (var i = 0; i < member.availability.length; i++) {
+      member.availability[i] = Math.floor(Math.random() * 2) < 0.5 ? 'No' : 'Si';
+      console.log(hours[i], member.availability[i]);
+    }
   }
-};
+}
 
-showProducts(products);
+randomSchedule(myTeam, WORK_HOURS);
 
-var calcTotal = (productList) => {
-  var allSubtotal = 0;
-  var allTaxes = 0;
-  var allTotal = 0;
-  var subtotal = 0;
-  var taxes = 0;
-  var total = 0;
+var disponibility = (member, hours) => {
+  teamLeader = member[0];
+  for (var i = 0; i < teamLeader.availability.length; i++) {
+    if (teamLeader.availability[i] === 'Si') {
+      for (var control = 1; control < member.length; control++) {
+        if (member[control].availability[i] === 'No') break;
+      }
+      if (control === member.length) return console.log("Hueco encontrado en el horario:", hours[i]);
+    }
 
-  for (var product of productList) {
-    var subtotal = product.units * product.price;
-    var taxes = (subtotal * product.tax) / 100;
-    var total = subtotal + taxes;
-
-    allSubtotal = allSubtotal + subtotal;
-    allTaxes = allTaxes + taxes;
-    allTotal = allTotal + total;
   }
-  document.getElementById("subtotal").innerHTML = allSubtotal.toFixed(2) + "€";
-  document.getElementById("taxes").innerHTML = allTaxes.toFixed(2) + "€";
-  document.getElementById("total").innerHTML = allTotal.toFixed(2) + "€";
-};
-// product.units == 0 ? 0 : product.units;
-// var disableButton = product.units == 0 ? true : false;
-// document.getElementById("button-calculate").disabled = disableButton;
+  console.log("Lo siento, no hay hueco disponible en el equipo.");
+}
+
+disponibility(myTeam, WORK_HOURS);
